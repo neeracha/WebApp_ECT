9c56689a7ade4f3e3e42e15df4363b2a3b266d9c
neeracha <neeracha1842@gmail.com>
2022-11-01 16:11:36 +0700

    work7

--- index.php
+++ index.php
@@ -1,87 +1,93 @@
 <?php
-session_start();
+    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webborad KakKak</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <title>Webboard ANYA</title>
 </head>
 <?php
-if (!isset($_SESSION['id'])) {
+    if(!isset($_SESSION['id'])){ 
 ?>
+<body>
+    <div>
+    <div class="container">
+    <h1 style="text-align:center;"><center>Webboard ANYA</center></h1>
+    <?php include "nav.php";?>
+    <br>
+    <div class="d-flex">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="dropdown">
+                <button class="btn btn-light dropdown-toggle btn-sm"
+                type="button" id="dropdown2" data-bs-toggle="dropdown"
+                aria-expanded="false">--ทั้งหมด--</button>
+                <ul class="dropdown-menu" aria-labelledby="dropdown2">
+                    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+                </ul>
 
-    <body>
-        <!--<h1><center>Webborad KakKak</center></h1> -->
-        <h1 style="text-align: center;">Webborad KakKak</h1>
-        <!--inline-->
-        <hr>
-        หมวดหมู่:
-        <select name="category">
-            <option value="all">--ทั้งหมด--</option>
-            <option value="general">--เรื่องทั่วไป--</option>
-            <option value="student">--เรื่องเรียน--</option>
-        </select>
-        <a href="login.php" style="float: right;">เข้าสู่ระบบ</a>
-        <ul>
-            <?php
-            for ($i = 1; $i <= 10; $i++) {
-                echo "<li><a href=post.php?id=$i>กระทู้ที่ $i</a></li>";
-            }
-            ?>
-            <!--ส่งแแบบ get
-        <li><a href="post.php?id=1">กระทู้ที่ 1</a></li>
-        <li><a href="post.php?id=2">กระทู้ที่ 2</a></li>
-        <li><a href="post.php?id=3">กระทู้ที่ 3</a></li>
-        <li><a href="post.php?id=4">กระทู้ที่ 4</a></li>
-        <li><a href="post.php?id=5">กระทู้ที่ 5</a></li>-->
-        </ul>
-
-    </body>
-<?php } else { ?>
-
-    <body>
-        <!--<h1><center>Webborad KakKak</center></h1> -->
-        <h1 style="text-align: center;">Webborad KakKak</h1>
-        <!--inline-->
-        <hr>
-        หมวดหมู่:
-        <select name="category">
-            <option value="all">--ทั้งหมด--</option>
-            <option value="general">--เรื่องทั่วไป--</option>
-            <option value="student">--เรื่องเรียน--</option>
-        </select>
-
-        <div style="float:right">
-            ผู้ใช้งานระบบ : <?php echo $_SESSION['username'] ?>&nbsp;&nbsp;
-            <a href="logout.php">ออกจากระบบ</a>
+            </span>
         </div>
-        <br>
-        <a href="newpost.php">สร้างกระทู้ใหม่</a>
+    </div>
+    <br>
+    <table class="table table-striped">
+        <?php 
+            for($i=1;$i<=10;$i++){
+                echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้ที่ $i</a></td></tr>" ;
+        }
+        ?>
+    </table>
+    </div>
+</body>
+<?php } else{ ?>
+    <body>
+        <div class="container">
+    <h1><center>Webboard Meaw</center></h1>
+    <?php include "nav.php";?>
 
-        <ul>
-            <?php
-            for ($i = 1; $i <= 10; $i++) {
-                echo "<li>";
-                echo "<a href=post.php?id=$i>กระทู้ที่ $i</a>";
-                if ($_SESSION['role'] == 'a') {
-                    echo "&nbsp;&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+    <div class="d-flex justify-content-between mt-3">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="dropdown">
+                <button class="btn btn-light dropdown-toggle btn-sm"
+                type="button" id="dropdown2" data-bs-toggle="dropdown"
+                aria-expanded="false">--ทั้งหมด--</button>
+                <ul class="dropdown-menu" aria-labelledby="dropdown2">
+                    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+                </ul>
+
+            </span>
+        </div>
+        <div>
+            <a href="newpost.php" class="btn btn-success btn-sm"><i class="bi bi-plus"></i> สร้างกระทู้ใหม่</a>
+        </div>
+    </div>
+    <br>
+    <table class="table table-striped">
+        <?php 
+            for($i=1;$i<=10;$i++){
+                echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้ที่ $i</a></td>" ;
+                if($_SESSION["role"]=="a"){
+                    echo "<td><a herf=delete.php?id=$i class='btn btn-danger btn-sm'>
+                    <i class='bi bi-trash'></i></a></td>";
                 }
-                echo "</li>";
-            }
-            ?>
-            <!--ส่งแแบบ get
-            <li><a href="post.php?id=1">กระทู้ที่ 1</a></li>
-            <li><a href="post.php?id=2">กระทู้ที่ 2</a></li>
-            <li><a href="post.php?id=3">กระทู้ที่ 3</a></li>
-            <li><a href="post.php?id=4">กระทู้ที่ 4</a></li>
-            <li><a href="post.php?id=5">กระทู้ที่ 5</a></li>-->
-        </ul>
-
-    </body>
+                echo "</tr>";
+        }
+        ?>
+    </table>
+    </div>
+</body>
 <?php } ?>
-
 </html>
--- verify.php
+++ verify.php
@@ -1,52 +1,28 @@
-    <?php
+<?php
     session_start();
-    if (isset($_SESSION['id'])) {
+    if(isset($_SESSION["username"]) && $_SESSION["id"]==session_id()){
         header("location:index.php");
-        die();
-    }
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
+        die();}
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Verify</title>
-    </head>
-
-    <body>
-        <h1 align="center">Webboard Kakkak</h1>
-        <hr>
-        <div align="center">
-            <!--   เข้าสู่ระบบด้วย<br>
-        Login = <?php //echo $_POST["login"];
-                ?><br>
-        Password = <?php // echo $_POST["pwd"];
-                    ?> -->
-            <?php
-            // echo "Login = ".$_POST["login"]."<br>";
-            //echo "Password = ".$_POST["pwd"];
-            ?>
-            <?php
-            if ($_POST["login"] == "admin" && $_POST["pwd"] == "ad1234") {
-                echo "ยินดีต้อนรับคุณ ADMIN";
-                $_SESSION['username'] = 'admin';
-                $_SESSION['role'] = 'a';
-                $_SESSION['id'] = session_id();
-            } else if ($_POST["login"] == "member" && $_POST["pwd"] == "mem1234") {
-                echo "ยินดีต้อนรับคุณ MEMBER";
-                $_SESSION['username'] = 'member';
-                $_SESSION['role'] = 'm';
-                $_SESSION['id'] = session_id();
-            } else {
-                echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
-            }
-            ?>
-        </div>
-        <div align="center">
-            <a href="index.php">กลับสู่หน้าหลัก</a>
-        </div>
-    </body>
-
-    </html>
+      
+    $u = $_POST['login'];
+    $p = $_POST['pwd']; 
+    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    $sql="SELECT * FROM user where login='$u' and password=sha1('$p')";
+    $result=$conn->query($sql);
+    if ($result->rowCount()==1) {
+        $data=$result->fetch(PDO::FETCH_ASSOC);
+            $_SESSION["username"]=$data["login"];
+            $_SESSION["role"]=$data["role"];
+            $_SESSION["user_id"]=$data["id"];
+            $_SESSION["id"]=session_id();
+            header("location:index.php");
+            die();
+        } 
+        else {
+            $_SESSION["error"]=1;
+            header("location:login.php");
+            die();
+        }
+        $conn=null;
+        ?>
